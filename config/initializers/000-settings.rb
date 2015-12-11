9fed4fbcbb360ffc368b47784e8afca004f005df
Guilherme Pereira <guilherme.pereira@ndrive.com>
2015-12-11 16:57:16 +0000

    Refactors initializers

--- config/initializers/000-settings.rb
+++ config/initializers/000-settings.rb
@@ -1,15 +1,35 @@
-module StravaCollector
-  class Config
-    attr_accessor :strava
+require 'json'
+require 'ostruct'
+require 'delegate'
 
+module Configurator
+  class Configuration < SimpleDelegator
     def initialize filename
-      JSON.parse(File.read(filename)).each do |key, value|
-        send("#{key.tr('-', '_')}=", value.deep_symbolize_keys)
-      end
+      hash = JSON.parse(File.read(filename))
+
+      hash = Hash[hash.map do |k, v|
+        [k.underscore, v.deep_symbolize_keys]
+      end]
+
+      __setobj__ OpenStruct.new(hash)
     end
   end
 
-  def self.config
-    @c ||= Config.new ::Rails.root.join('config', 'settings.json')
+  def self.config filename
+    Module.new do
+      class << self
+        def included base
+          base.extend self
+        end
+      end
+
+      define_method(:config) do
+        @config ||= Configuration.new(filename)
+      end
+    end
   end
+end
+
+module StravaCollector
+  include Configurator.config(::Rails.root.join('config', 'settings.json'))
 end

c5a128fa60d072b51e02f5e0558d2660d83f93ff
Guilherme Pereira <guilherme.pereira@ndrive.com>
2015-12-11 16:58:16 +0000

    Adds current user on before filter (temporary stuff)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,17 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter do
+    @current_user ||= current_user
+  end
+
+  protected
+  def current_user
+    Athlete.new(strava_client.retrieve_current_athlete)
+  end
+
+  def strava_client
+    @strava_client ||= Strava::Api::V3::Client.new(access_token: StravaCollector.config.strava[:token])
+  end
 end
